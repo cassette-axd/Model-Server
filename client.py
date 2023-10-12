a5edd4dee8829395b09fce891caadb526e9901aa
quinferia <joaquincarlosferia@gmail.com>
2023-10-12 14:32:55 +0000

    Partially committed Part 3

--- client.py
+++ client.py
@@ -1,14 +1,36 @@
 import modelserver_pb2_grpc, modelserver_pb2
 import grpc
 import threading
+import sys
+import pandas as pd
 
-channel = grpc.insecure_channel("localhost:5440")
+
+channel = grpc.insecure_channel("localhost:" + sys.argv[0])
 stub = modelserver_pb2_grpc.ModelServerStub(channel)
 
-resp = stub.SetCoefs(modelserver_pb2.SetCoefsRequest(coefs = [1.0,2.0,3.0]))
+coefsList =  list(map(int, sys.argv[1].split(",")))
+resp = stub.SetCoefs(modelserver_pb2.SetCoefsRequest(coefs = coefsList))
+mainThread = threading.Thread()
+mainThread.start()
+def loopCSV(file):
+    df = pd.read_csv(file)
+    total = 0
+    totalHits = 0
+    for index, row in df.iterrows:
+        y, hit = stub.Prediction(row)
+        if (hit):
+            totalHits += 1
+        total += 1
+    print(totalHits/total)
+    totalMisses = totalHits-total
 
-def loopCSV():
-    
-t1 = threading.Thread(target=loopCSV)
-t2 = threading.Thread(target=loopCSV)
-t3 = threading.Thread(target=loopCSV)
+threads = []
+for n in range(2, sys.argv.count-1):
+    t = threading.Thread(target=loopCSV, args=sys.argv[n])
+    t.start()
+    threads.append(t)
+
+for i in threads:
+    threads[i].join()
+
+mainThread.join()
